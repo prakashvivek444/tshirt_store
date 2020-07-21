7b1781ff0bb0827bc0a5a0a08b7b4887609d3fbb
Vivek Prakash <prakashvivek444@gmail.com>
2020-07-21 19:23:43 +0530

    fixed error in models/user.js in line 59

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -69,7 +69,7 @@ exports.signout = (req ,res) => {
 exports.isSignedIn = expressJwt({
     secret: process.env.SECRET,
     userProperty: "auth"
-})
+});
 
  //custom middleware
  
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { check , validationResult } = require("express-validator");
 
-const {signout , signup , signin} = require("../controllers/auth");
+const {signout , signup , signin ,isSignedIn} = require("../controllers/auth");
 router.get("/signout", signout);
 router.post("/signup",[
     check("name").isLength({min:4}).withMessage("Name must be at least 5 chars long"),
@@ -14,4 +14,5 @@ router.post("/signin",[
     check("email").isEmail().withMessage("Email is required"),
     check("password").isLength({min:1}).withMessage("Password field is required")
 ], signin);
+
 module.exports = router;
