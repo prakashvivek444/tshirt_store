2cf3d6e36bca9ae4da36a09e24bb09b5a09b4af7
Vivek Prakash <prakashvivek444@gmail.com>
2020-08-07 18:58:14 +0530

    Wrote isAdmin and isAuthenticated middleware in controllers auth.js

--- projbackend/routes/user.js
+++ projbackend/routes/user.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const router = express.Router();
 
+const {getUserById,getUser} = require("../controllers/user");
+const {isSignedIn,isAdmin,isAuthenticated} = require("../controllers/auth");
+
+
+router.param("userId",getUserId);
+
+router.get("/user/:userId",getUser);
+
+
+
+module.exports = router;
